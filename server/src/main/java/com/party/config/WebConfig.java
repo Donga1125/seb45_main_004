3a69c3cb6c871b3b23caa96cf0701f251b0c5e94
Donga <96361421+Donga1125@users.noreply.github.com>
2024-04-30 17:15:03 +0900

    [BE]hotfix: cors 설정 추가

--- server/src/main/java/com/party/config/WebConfig.java
+++ server/src/main/java/com/party/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://celebee.kro.kr", "https://www.celebee.kro.kr")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
