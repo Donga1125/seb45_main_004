5bc2b1becfcd9e4e7c77455bdafd24fc5f4bb09f
Donga <96361421+Donga1125@users.noreply.github.com>
2024-04-30 15:30:46 +0900

    Update WebConfig.java

--- server/src/main/java/com/party/config/WebConfig.java
+++ server/src/main/java/com/party/config/WebConfig.java
@@ -3,7 +3,7 @@ package com.party.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
